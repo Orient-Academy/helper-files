9d417f60c14c0888974cd1379602b48a7dd10898
muradimanbayli <muradimanbayli@gmail.com>
2024-02-09 17:42:38 +0400

    first commit

---
+++ setup-ingress.sh
@@ -0,0 +1,44 @@
+##https://docs.aws.amazon.com/eks/latest/userguide/aws-load-balancer-controller.html
+
+## Creating an IAM OIDC provider for your cluster
+oidc_id=$(aws eks describe-cluster --name $cluster_name --query "cluster.identity.oidc.issuer" --output text | cut -d '/' -f 5)
+echo $oidc_id
+
+aws iam list-open-id-connect-providers | grep $oidc_id | cut -d "/" -f4
+
+eksctl utils associate-iam-oidc-provider --cluster $cluster_name --approve
+
+# Installing the AWS Load Balancer Controller add-on
+
+curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.5.4/docs/install/iam_policy.json
+
+aws iam create-policy \
+    --policy-name AWSLoadBalancerControllerIAMPolicy \
+    --policy-document file://iam_policy.json
+
+eksctl create iamserviceaccount \
+  --cluster=<cluster_name> \
+  --namespace=kube-system \
+  --name=aws-load-balancer-controller \
+  --role-name AmazonEKSLoadBalancerControllerRole \
+  --attach-policy-arn=arn:aws:iam::<accountId>:policy/AWSLoadBalancerControllerIAMPolicy \
+  --approve
+
+kubectl apply \
+    --validate=false \
+    -f https://github.com/jetstack/cert-manager/releases/download/v1.13.3/cert-manager.yaml
+
+curl -Lo v2_5_4_full.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.5.4/v2_5_4_full.yaml
+
+
+curl -Lo v2_5_4_full.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.5.4/v2_5_4_full.yaml
+
+sed -i.bak -e '596,604d' ./v2_5_4_full.yaml
+
+sed -i.bak -e 's|your-cluster-name|<cluster_name>|' ./v2_5_4_full.yaml
+
+kubectl apply -f v2_5_4_full.yaml
+
+curl -Lo v2_5_4_ingclass.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.5.4/v2_5_4_ingclass.yaml
+
+kubectl apply -f v2_5_4_ingclass.yaml
