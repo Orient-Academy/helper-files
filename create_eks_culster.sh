d90e4dbb40d12cbf10b995dd3be70fb907f86d62
Murad Imanbayli <muradimanbayli@gmail.com>
2024-11-04 02:39:28 +0400

    Update create_eks_culster.sh

--- create_eks_culster.sh
+++ create_eks_culster.sh
@@ -1,1 +1,1 @@
-eksctl create cluster --name orient --node-type t2.xlarge --nodes 2
+eksctl create cluster --name orient --node-type t2.xlarge --vpc-from-kops-cluster default --version 1.27 --nodes 2
